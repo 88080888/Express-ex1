c9db2bba1363c21d10c6ae68a2e2c8fe5a2d2d80
Katarzyna <kkazimierczuk@gmail.com>
2020-10-05 19:15:29 +0200

    add dark layout

--- server.js
+++ server.js
@@ -1,29 +1,50 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
+app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './views/layouts', defaultLayout: 'main' }));
+app.set('view engine', '.hbs');
   
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });  
 
 app.get('/home', (req, res) => {
-  res.show('home.html');
+  res.render('home');
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', { layout: 'dark' });
+});
+
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+
+app.get('/history', (req, res) => {
+  res.render('history');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
 });
 
 app.get('/404.png', (req, res) => {
   res.sendFile(path.join(__dirname, '/404.png'));
+});
+
+app.get('/style.css', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/style.css'));
+});
+
+app.get('/dark.css', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/dark.css'));
 });
 
 app.use('/user', (req, res, next) => {
@@ -32,7 +53,7 @@ app.use('/user', (req, res, next) => {
 })
 
 app.use((req, res) => {
-  res.status(404).show('404.html');
+  res.status(404).render('404.hbs');
 })
 
 app.listen(3000, () => {
