1c29b6d4fb055451604b8f1e84950449c651932c
Katarzyna <kkazimierczuk@gmail.com>
2020-10-08 11:26:13 +0200

    add ProjectDesign placeholder

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.set('view engine', '.hbs');
 
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.json());
+app.use(express.static(path.join(__dirname + '/public')));
   
 app.get('/', (req, res) => {
   res.render('index');
@@ -56,7 +56,7 @@ app.post('/contact/send-message', (req, res) => {
   const { author, sender, title, message, file } = req.body;
 
   if(author && sender && title && message && file) {
-    res.render('contact', { isSent: true, });
+    res.render('contact', { isSent: true, file:file });
   }
   else {
     res.render('contact', { isError: true })
